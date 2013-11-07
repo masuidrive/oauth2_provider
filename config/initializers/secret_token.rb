ed12115ed8e9c041798639f3c6ec93c02a868c71
Yuichiro MASUI <masui@masuidrive.jp>
2013-11-07 15:35:56 +0900

    rails new oauth2_provider on Rails 4.0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oauth2Provider::Application.config.secret_key_base = 'a8a41e8656d7cf9aec7da5f3ac31414a67b1c383d383950fb6bb86d86233ade4240a5fdae19d0e05cea2038b0b576be3f9dfb3a882deee2e30013e916e98aea2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Oauth2Provider::Application.config.session_store :cookie_store, key: '_oauth2_provider_session'
